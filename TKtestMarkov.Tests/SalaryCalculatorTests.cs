535325a1d56c93c57f990f47d08d068ab24fe631
Андрей Марков <e.markova8484@gmail.com>
2026-04-02 14:06:25 +0300

    Автотесты

---
+++ TKtestMarkov.Tests/SalaryCalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TKtestMarkov; 
+using System;
+
+namespace TKtestMarkov.Tests
+{
+    [TestClass]
+    public class SalaryCalculatorTests
+    {
+        [TestMethod]
+        public void CalculateSalary_ValidInput_ReturnsCorrectResult()
+        {
+            double result = SalaryCalculator.CalculateSalary(40, 150);
+            Assert.AreEqual(6000, result);
+        }
+
+        [TestMethod]
+        public void CalculateSalary_NegativeHours_ThrowsException()
+        {
+            // Assert.ThrowsException доступен только с MSTest
+            object value = Assert.ThrowsException<ArgumentException>(() =>
+            {
+                SalaryCalculator.CalculateSalary(-5, 150);
+            });
+        }
+
+        [TestMethod]
+        public void CalculateTax_WithTax_ReturnsCorrectTax()
+        {
+            double salary = 6000;
+            double tax = SalaryCalculator.CalculateTax(salary, true);
+            Assert.AreEqual(780, tax); // 13% от 6000
+        }
+
+        [TestMethod]
+        public void CalculateTax_WithoutTax_ReturnsZero()
+        {
+            double salary = 6000;
+            double tax = SalaryCalculator.CalculateTax(salary, false);
+            Assert.AreEqual(0, tax);
+        }
+    }
+}
---
+++ TKtestMarkov.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace TKtestMarkov.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
